97dc35f40758a441fe9830af9b3e698d0aa10af0
Nedim Kaçan <ndmkcn1453@gmail.com>
2023-07-22 18:18:56 +0300

    Creating-Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular1';
+  title = 'Angular Projesi yapıyoruz bu data değişkenden geliyor.';
+  name = 'Mehmet';
+  surname = 'Kara';
+  job = "Software Developer";
+  age = 30;
 }
